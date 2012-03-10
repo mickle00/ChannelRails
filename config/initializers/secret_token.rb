ac449942dc161337647c4349a8156dcf112adf83
Michael Stewart <mickle00@gmail.com>
2012-03-09 16:19:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsChannels::Application.config.secret_token = 'e88ba29ccdcdc61606be321d4692aeb2c875cf5e2136969e3675104a922581c0cd309da171b8c766e7386e7647ec0d60d31ab79785899927395803fd2061cb2f'
---
+++ db/migrate/20120309222859_create_channels.rb
@@ -0,0 +1,11 @@
+class CreateChannels < ActiveRecord::Migration
+  def change
+    create_table :channels do |t|
+      t.string :name
+      t.integer :number
+      t.boolean :hd
+
+      t.timestamps
+    end
+  end
+end
